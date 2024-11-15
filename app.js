cb81d51c91fbc11c7da778bf37ff89e7d5215032
krayush1109 <hellotoayush@gmail.com>
2024-11-15 11:21:17 +0530

    1. Removed Junk Files & 2. Setup db_connection

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+require('dotenv').config({ path: './.env' });
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index.route');
+var usersRouter = require('./routes/users.route');
+
+// database connection
+var db = require('./models/db_connection');
+// console.log(db);
+db.connectDB();
 
 var app = express();
 
@@ -23,12 +30,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ models/db_connection.js
@@ -0,0 +1,15 @@
+// Import the mongoose library
+const mongoose = require('mongoose');
+
+// Export an asynchronous function to connect to the MongoDB database
+exports.connectDB = async () => {
+    try {
+        // Attempt to connect to the database using the URI from environment variables
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        // Log a success message with the host of the connection
+        console.log("DB Connected : ", conn.connection.host);
+    } catch (err) {
+        // Log an error message if the connection fails
+        console.error("Connection Error : ", err.message);
+    }
+};
